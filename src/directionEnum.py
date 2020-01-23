acb00b0f6f549ec4953bc7f6d4a6818eace7f643
jan <fleisch.ja@gmail.com>
2020-01-23 21:11:20 +0100

    added compelte path planing to the service

---
+++ src/directionEnum.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Direction(Enum):
+    Neutral = 0 
+    NorthWest = 1
+    North = 2
+    NorthEast = 3
+    East = 4
+    SouthEast = 5
+    South = 6
+    SouthWest = 7
+    West = 8
